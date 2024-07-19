9013c40437c8e65bbde42664329a891756ef0d4c
kayumov1554 <167546948+kayumov1554@users.noreply.github.com>
2024-07-19 22:28:08 +0500

    ggg

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ document.getElementById("radius3").style.display = "none";
 document.getElementById("clear").style.display = "none";
 document.getElementById("other").style.display = "none";
 document.getElementById("add").style.display = "none";
+document.getElementById("images").style.display = "none";
+document.getElementById("img1").style.display = "none";
+document.getElementById("img2").style.display = "none";
+document.getElementById("img3").style.display = "none";
 
 function getStarted() {
   document.getElementById("title").style.display = "none";
@@ -24,58 +28,111 @@ function getStarted() {
   document.getElementById("clear").style.display = "block";
   document.getElementById("other").style.display = "block";
   document.getElementById("add").style.display = "none";
+  document.getElementById("images").style.display = "none";
+  document.getElementById("img1").style.display = "none";
+  document.getElementById("img2").style.display = "none";
+  document.getElementById("img3").style.display = "none";
 }
 
 function style1() {
   document.getElementById("image").style.filter = "grayscale(100%)";
-  document.getElementById("add").style.display = "block";
+  document.getElementById("add").style.display = "none";
+  document.getElementById("img1").style.filter = "grayscale(100%)";
+  document.getElementById("img2").style.filter = "grayscale(100%)";
+  document.getElementById("img3").style.filter = "grayscale(100%)";
 }
 
 function style2() {
   document.getElementById("image").style.filter = "contrast(150%)";
-  document.getElementById("add").style.display = "block";
+  document.getElementById("img1").style.filter = "contrast(150%)";
+  document.getElementById("img2").style.filter = "contrast(150%)";
+  document.getElementById("img3").style.filter = "contrast(150%)";
+  document.getElementById("add").style.display = "none";
 }
 function style3() {
   document.getElementById("image").style.filter = "blur(3px)";
-  document.getElementById("add").style.display = "block";
+  document.getElementById("img1").style.filter = "blur(3px)";
+  document.getElementById("img2").style.filter = "blur(3px)";
+  document.getElementById("img3").style.filter = "blur(3px)";
+  document.getElementById("add").style.display = "none";
 }
 
 function radius1() {
   document.getElementById("image").style.borderRadius = "30px";
-  document.getElementById("add").style.display = "block";
+  document.getElementById("img1").style.borderRadius = "30px";
+  document.getElementById("img2").style.borderRadius = "30px";
+  document.getElementById("img3").style.borderRadius = "30px";
+  document.getElementById("add").style.display = "none";
 }
 function radius2() {
   document.getElementById("image").style.borderRadius = "50px";
-  document.getElementById("add").style.display = "block";
+  document.getElementById("img1").style.borderRadius = "50px";
+  document.getElementById("img2").style.borderRadius = "50px";
+  document.getElementById("img3").style.borderRadius = "50px";
+  document.getElementById("add").style.display = "none";
 }
 
 function radius3() {
   document.getElementById("image").style.borderRadius = "70px";
-  document.getElementById("add").style.display = "block";
+  document.getElementById("img1").style.borderRadius = "70px";
+  document.getElementById("img2").style.borderRadius = "70px";
+  document.getElementById("img3").style.borderRadius = "70px";
+  document.getElementById("add").style.display = "none";
 }
 
 function filter() {
   document.getElementById("image").style.filter = "none";
   document.getElementById("image").style.borderRadius = "0px";
+  document.getElementById("img2").style.borderRadius = "0px";
+  document.getElementById("img1").style.borderRadius = "0px";
+  document.getElementById("img3").style.borderRadius = "0px";
+  document.getElementById("img2").style.filter = "blur(0px)";
+  document.getElementById("img1").style.filter = "blur(0px)";
+  document.getElementById("img3").style.filter = "blur(0px)";
+  document.getElementById("img2").style.filter = "contrast(0%)";
+  document.getElementById("img1").style.fillter = "contrast(0%)";
+  document.getElementById("img3").style.filter = "contrast(0%)";
+  document.getElementById("img2").style.filter = "grayscale(0%)";
+  document.getElementById("img1").style.fillter = "grayscale(0%)";
+  document.getElementById("img3").style.filter = "grayscale(0%)";
   document.getElementById("text").style.display = "none";
   document.getElementById("addimg").style.display = "block";
   document.getElementById("add").style.display = "none";
 }
-const images = [
-  "/img/manzara.jpg",
-  "/img/manzara2.jpg",
-  "/img/manzara3.jpg",
-  "/img/manzara4.jpg",
-];
-let currentIndex = 0;
 
 function other() {
-  const image = document.getElementById("image");
-  currentIndex = (currentIndex + 1) % images.length;
-  image.src = images[currentIndex];
+  document.getElementById("images").style.display = "flex";
+  document.getElementById("images").style.gap = "10px";
+  document.getElementById("btnimg").style.display = "block";
+  document.getElementById("btnimg2").style.display = "block";
+  document.getElementById("btnimg3").style.display = "block";
+  document.getElementById("done").style.display = "block";
 }
-function add() {
-  document.getElementById("text").innerHTML = "comming soon...";
-  document.getElementById("text").style.display = "block";
-  document.getElementById("addimg").style.display = "none";
+
+function btnimg() {
+  document.getElementById("image").style.display = "none";
+  document.getElementById("img1").style.display = "block";
+  document.getElementById("img3").style.display = "none";
+  document.getElementById("img2").style.display = "none";
 }
+function btnimg2() {
+  document.getElementById("image").style.display = "none";
+  document.getElementById("img2").style.display = "block";
+  document.getElementById("img1").style.display = "none";
+  document.getElementById("img3").style.display = "none";
+}
+
+function btnimg3() {
+  document.getElementById("image").style.display = "none";
+  document.getElementById("img2").style.display = "none";
+  document.getElementById("img1").style.display = "none";
+  document.getElementById("img3").style.display = "block";
+}
+
+function done() {
+  document.getElementById("btnimg").style.display = "none";
+  document.getElementById("btnimg2").style.display = "none";
+  document.getElementById("btnimg3").style.display = "none";
+  document.getElementById("done").style.display = "none";
+}
+new WOW().init();
