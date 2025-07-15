e95ea357a1b9a59b4609a52f44a79cf2987ed839
kayumov1554 <167546948+kayumov1554@users.noreply.github.com>
2025-07-16 04:44:27 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const { Telegraf } = require('telegraf');
+const dictionary = require('./dictionary'); // lug'at alohida faylda
+
+const bot = new Telegraf('7512880109:AAFXlYoxQU3xPrt60w4vJCY4LzCLqhu5nRY');
+
+// Matn yuborilganda ishlaydi
+bot.on('text', (ctx) => {
+  const input = ctx.message.text.trim().toLowerCase();
+
+  const match = dictionary.find(item =>
+    item.uz.toLowerCase() === input || item.ko === input
+  );
+
+  if (match) {
+    ctx.reply(`🇺🇿 ${match.uz}\n🇰🇷 ${match.ko} (${match.roman})`);
+  } else {
+    ctx.reply("Kechirasiz, bu so‘z uchun tarjima topilmadi.");
+  }
+});
+
+bot.launch();
+console.log("Bot ishga tushdi...");
